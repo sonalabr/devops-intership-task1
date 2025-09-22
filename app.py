5eec06f34a363638c798854318e7cf30d2716f0b
sonalabr <60999730+sonalabr@users.noreply.github.com>
2025-09-22 14:53:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hi I am Python Flask App from CI/CD with GitHub Actions and Dockerhub!"
+    return "Hi I am Python Flask App from CI/CD with GitHub Actions and Dockerhub"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
